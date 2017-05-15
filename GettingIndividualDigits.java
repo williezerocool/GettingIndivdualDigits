5a3ebaab2dab48b88a79b60d1836572a8643a281
Willie Suggs <klbooker09@gmail.com>
2017-05-14 23:26:42 -0500

    Add files via upload

---
+++ GettingIndividualDigits.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gettingindividualdigits;
+
+/**
+ *
+ * @author kendrabooker
+ */
+public class GettingIndividualDigits {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        for(int i=1; i<=10; i++){
+        
+            for(int j=0; j <= 9; j++){
+            
+                System.out.println(i + "" + j + ", " + i + " + " + j + " = " + " " + (i + j) );
+            }
+        }
+    }
+    
+}
